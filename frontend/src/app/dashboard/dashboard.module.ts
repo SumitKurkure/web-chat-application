ba7a0c3e205fd5114ec6f232fcb14022bf7855a8
SumitKurkureKL <107413868+SumitKurkureKL@users.noreply.github.com>
2023-07-03 11:48:10 +0530

    chat component update

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -6,16 +6,19 @@ import { DashboardComponent } from './dashboard.component';
 import { ChatComponent } from './chat/chat.component';
 import { VideoComponent } from './video/video.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { ChatDashboardComponent } from './chat/chat-dashboard/chat-dashboard.component';
 
 @NgModule({
   declarations: [
     ChatComponent,
-    VideoComponent
+    VideoComponent,
+    ChatDashboardComponent
   ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
     BrowserModule,
+    
   ]
 })
 export class DashboardModule { }
