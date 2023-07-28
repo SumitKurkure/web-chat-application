4fcdbc38ec181aeeb2f1d99f5d73221c4f38d949
SumitKurkureKL <107413868+SumitKurkureKL@users.noreply.github.com>
2023-07-28 16:51:47 +0530

    chat dashboard click on button size changin with setting component added

--- frontend/src/app/dashboard/chat/settings/settings.component.ts
+++ frontend/src/app/dashboard/chat/settings/settings.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  public settingMenu = [
+    {
+      label: 'Profile Picture',
+      value: 'account',
+      icon : 'fa fa-user'
+    },
+    {
+      label: 'Delete Account',
+      value: 'delete_account',
+      icon : 'fa fa-trash'
+    },
+    {
+      label : 'Block Contact',
+      value : 'block_account',
+      icon : 'fa fa-ban'
+    },
+    {
+      label : 'Delete Chats',
+      value : 'delete_chats',
+      icon : 'fa fa-comment-slash'
+    },
+    {
+      label: 'Logout',
+      value : 'logout',
+      icon : 'fa fa-arrow-right-from-bracket'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
